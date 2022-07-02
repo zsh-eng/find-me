46cf59fc9fe8179db04d19e889b6e8ac338d02a5
sheng <49238630+zsh-eng@users.noreply.github.com>
2022-07-02 19:56:47 +0800

    chore: Set up boilerplate

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <section className="App-section"></section>
+    </div>
+  );
+}
+
+export default App;
